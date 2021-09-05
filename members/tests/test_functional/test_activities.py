e222b18cf1353826b5768c993cc1dab5a82051a7
Jarl Friis <jarl@softace.dk>
2021-09-05 21:03:32 +0200

    offentliggørelse af sider

--- members/tests/test_functional/test_activities.py
+++ members/tests/test_functional/test_activities.py
@@ -63,6 +63,110 @@ class ActivitiesTest(StaticLiveServerTestCase):
         self.browser.save_screenshot("test-screens/activities_list_final.png")
         self.browser.quit()
 
+    def test_activities(self):
+        # Loads the activities
+        self.browser.find_element_by_link_text("Arrangementer").click()
+        WebDriverWait(self.browser, 10).until(
+            lambda d: d.execute_script("return 'initialised'")
+        )
+        self.browser.save_screenshot("test-screens/activities_list.png")
+
+        # Check that the page contains all participating activities
+        activity_names = [
+            e.text
+            for e in self.browser.find_elements_by_xpath(
+                "//section[@id='participation']/table/tbody/tr/td[@data-label='Aktivitet']"
+            )
+        ]
+        self.assertEqual(2, len(activity_names))
+        self.assertIn(
+            self.activities["ARRANGEMENT"]["participate"].name, activity_names
+        )
+        self.assertIn(self.activities["FORLØB"]["participate"].name, activity_names)
+
+        # Check that the page contains all activities
+        activity_names = [
+            e.text
+            for e in self.browser.find_elements_by_xpath(
+                "//section[@id='open_activities']/table/tbody/tr/td[@data-label='Aktivitet']"
+            )
+        ]
+        self.assertEqual(2, len(activity_names))
+        self.assertIn(self.activities["ARRANGEMENT"]["recent"].name, activity_names)
+        self.assertIn(self.activities["FORLØB"]["recent"].name, activity_names)
+
+    def test_membership(self):
+        # Loads the members
+        self.browser.find_element_by_link_text("Medlemskaber").click()
+        WebDriverWait(self.browser, 10).until(
+            lambda d: d.execute_script("return 'initialised'")
+        )
+        self.browser.save_screenshot("test-screens/membership_list.png")
+
+        # Check that the page contains all participating activities
+        activities = self.browser.find_elements_by_xpath(
+            "//section[@id='participation']/table/tbody/tr"
+        )
+        self.assertEqual(1, len(activities))
+        self.assertEqual(
+            self.activities["FORENINGSMEDLEMSKAB"]["participate"].name,
+            activities[0].find_element_by_xpath("td[@data-label='Aktivitet']").text,
+        )
+        self.assertEqual(
+            self.activities["FORENINGSMEDLEMSKAB"]["participate"].union.name,
+            activities[0].find_element_by_xpath("td[@data-label='Forening']").text,
+        )
+
+        # Check that the page contains all activities
+        activities = self.browser.find_elements_by_xpath(
+            "//section[@id='open_activities']/table/tbody/tr"
+        )
+        self.assertEqual(1, len(activities))
+        self.assertEqual(
+            self.activities["FORENINGSMEDLEMSKAB"]["recent"].name,
+            activities[0].find_element_by_xpath("td[@data-label='Aktivitet']").text,
+        )
+        self.assertEqual(
+            self.activities["FORENINGSMEDLEMSKAB"]["recent"].union.name,
+            activities[0].find_element_by_xpath("td[@data-label='Forening']").text,
+        )
+
+    def test_supportmembership(self):
+        # Loads the members
+        self.browser.find_element_by_link_text("Støttemedlemskaber").click()
+        WebDriverWait(self.browser, 10).until(
+            lambda d: d.execute_script("return 'initialised'")
+        )
+        self.browser.save_screenshot("test-screens/supportmembership_list.png")
+
+        # Check that the page contains all participating activities
+        activities = self.browser.find_elements_by_xpath(
+            "//section[@id='participation']/table/tbody/tr"
+        )
+        self.assertEqual(1, len(activities))
+        self.assertEqual(
+            self.activities["STØTTEMEDLEMSKAB"]["participate"].name,
+            activities[0].find_element_by_xpath("td[@data-label='Aktivitet']").text,
+        )
+        self.assertEqual(
+            self.activities["STØTTEMEDLEMSKAB"]["participate"].union.name,
+            activities[0].find_element_by_xpath("td[@data-label='Forening']").text,
+        )
+
+        # Check that the page contains all activities
+        activities = self.browser.find_elements_by_xpath(
+            "//section[@id='open_activities']/table/tbody/tr"
+        )
+        self.assertEqual(1, len(activities))
+        self.assertEqual(
+            self.activities["STØTTEMEDLEMSKAB"]["recent"].name,
+            activities[0].find_element_by_xpath("td[@data-label='Aktivitet']").text,
+        )
+        self.assertEqual(
+            self.activities["STØTTEMEDLEMSKAB"]["recent"].union.name,
+            activities[0].find_element_by_xpath("td[@data-label='Forening']").text,
+        )
+
     def test_activities_as_member(self):
         # Login
         log_in(self, self.member.person)
--- members/views/Activities.py
+++ members/views/Activities.py
@@ -1,54 +1,57 @@
 import datetime
 from django.shortcuts import render
 from django.utils import timezone
-from django.contrib.auth.decorators import login_required, user_passes_test
 
 from members.models.activity import Activity
 from members.models.activityinvite import ActivityInvite
 from members.models.activityparticipant import ActivityParticipant
 from members.models import Person
-from members.utils.user import user_to_person, has_user
+from members.utils.user import user_to_person
 
 
-@login_required
-@user_passes_test(has_user, "/admin_signup/")
 def Activities(request):
-    family = user_to_person(request.user).family
-    invites = ActivityInvite.objects.filter(
-        person__family=family, expire_dtm__gte=timezone.now(), rejected_dtm=None
-    )
     open_activities = Activity.objects.filter(
         open_invite=True,
         signup_closing__gte=timezone.now(),
         activitytype__in=["FORLØB", "ARRANGEMENT"],
     ).order_by("zipcode")
-    participating = ActivityParticipant.objects.filter(
-        member__person__family=family,
-        activity__activitytype__in=["FORLØB", "ARRANGEMENT"],
-    ).order_by("-activity__start_date")
 
-    open_activities_with_persons = []
-    # augment open invites with the persons who could join it in the family
-    for curActivity in open_activities:
-        applicablePersons = Person.objects.filter(
-            family=family,  # only members of this family
-            birthday__lte=timezone.now()
-            - datetime.timedelta(days=curActivity.min_age * 365),  # old enough
-            birthday__gt=timezone.now()
-            - datetime.timedelta(days=curActivity.max_age * 365),  # not too old
-        ).exclude(
-            member__activityparticipant__activity=curActivity
-        )  # not already participating
+    family = None
+    invites = None
+    participating = None
+    open_activities_with_persons = None
+    if request.user.is_authenticated:
+        family = user_to_person(request.user).family
+        invites = ActivityInvite.objects.filter(
+            person__family=family, expire_dtm__gte=timezone.now(), rejected_dtm=None
+        )
+        participating = ActivityParticipant.objects.filter(
+            member__person__family=family,
+            activity__activitytype__in=["FORLØB", "ARRANGEMENT"],
+        ).order_by("-activity__start_date")
 
-        if applicablePersons.exists():
-            open_activities_with_persons.append(
-                {
-                    "id": curActivity.id,
-                    "name": curActivity.name,
-                    "department": curActivity.department,
-                    "persons": applicablePersons,
-                }
-            )
+        open_activities_with_persons = []
+        # augment open invites with the persons who could join it in the family
+        for curActivity in open_activities:
+            applicablePersons = Person.objects.filter(
+                family=family,  # only members of this family
+                birthday__lte=timezone.now()
+                - datetime.timedelta(days=curActivity.min_age * 365),  # old enough
+                birthday__gt=timezone.now()
+                - datetime.timedelta(days=curActivity.max_age * 365),  # not too old
+            ).exclude(
+                member__activityparticipant__activity=curActivity
+            )  # not already participating
+
+            if applicablePersons.exists():
+                open_activities_with_persons.append(
+                    {
+                        "id": curActivity.id,
+                        "name": curActivity.name,
+                        "department": curActivity.department,
+                        "persons": applicablePersons,
+                    }
+                )
 
     context = {
         "family": family,
